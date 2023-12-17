452271ed4a8e426f014f09b553933a4e73166ad5
Stefan <sminns1230@gmail.com>
2023-12-17 16:38:51 +0000

    Add playerInput and computerChoice

--- main.js
+++ main.js
@@ -1,16 +1,39 @@
+let playerPrompt = prompt("Choose either rock, paper or scissors:");
+
+function playerInput() {
+    let lowerPrompt = playerPrompt.toLowerCase();
+
+    if (lowerPrompt == 'rock') {
+        return 1;
+    } else if (lowerPrompt == 'paper') {
+        return 2;
+    } else if (lowerPrompt == 'scissors') {
+        return 3;
+    } else  {
+        console.log('Invalid option');
+    }
+}
+    
+console.log(playerInput());
+
 function getComputerChoice() {
-    let choiceArray = [
-        'Rock',
-        'Paper',
-        'Scissors'
-    ];
-    let randomIndex = Math.floor(Math.random() * choiceArray.length);
-    let randomChoice = choiceArray[randomIndex];
-    return randomChoice;
+    const choiceArray = ["rock", "paper", "scissors"];
+    let randomChoice = choiceArray[Math.floor(Math.random() * choiceArray.length)];
+
+    if (randomChoice == 'rock') {
+        return 1;
+    } else if (randomChoice == 'paper') {
+        return 2;
+    } else if (randomChoice == 'scissors') {
+        return 3;
+    }
 }
 
 console.log(getComputerChoice());
 
-// function gameRound(playerSelection, computerSelection) {
-    
-// }
+function gameRound(playerSelection, computerSelection) {
+    playerSelection = playerInput();
+    computerSelection = getComputerChoice();
+}
+
+console.log(gameRound())
